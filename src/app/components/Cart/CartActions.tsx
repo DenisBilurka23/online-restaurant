6a59774706e064aac7bb9483d1a411500eb8498c
DenisBilurka23 <vadya555555@gmail.com>
2024-01-13 17:04:20 -0500

    Main page contacts, checkout link and mobile-nav refactoring

--- src/app/components/Cart/CartActions.tsx
+++ src/app/components/Cart/CartActions.tsx
@@ -1,8 +1,16 @@
 import { type FC, useMemo } from 'react'
 import { type CartProductItem } from '../../../../types'
+import { useRouter } from 'next/navigation'
 
 const CartActions: FC<{ cart: CartProductItem[]; onClose: () => void }> = ({ cart, onClose }) => {
+	const router = useRouter()
 	const cartTotal = useMemo(() => cart.reduce((acc, product) => (acc += product.price * product.quantity), 0), [cart])
+
+	const handleCheckout = (): void => {
+		router.push(
+			'https://checkout.stripe.com/c/pay/cs_test_b1GSiDJbOZSpv5FZbm4CaMMFC4p336AcZIuFE4Rxs5epRDYhzfTDNfdXDW#fidkdWxOYHwnPyd1blpxYHZxWjA0SkprcTRHUUtMdzw9dn91XUhwaHFPXGpOT2FXVkczalNgYWZjMUFTMk5Hal1oS0BdS00zMV89S2thcUw0c19QQDZuVl9XPXBQNTB9dVYwSTVOYnw9MVxBNTVLfENfZ1xPfCcpJ2hsYXYnP34nYnBsYSc%2FJ2Q8YTA9NGcwKGFhMzwoMTYxMCg9NGZjKGY3ZDBjZmQ9ZzJhZGQzNzdgMicpJ2hwbGEnPydhNWY2YWc3YChkYT1nKDE8MjEoPGdmYCgyZD1kPDU0MjBmZDY8MzJgZGcnKSd2bGEnPycyPTAzPTM3YCgyNGY3KDE1Nj0oPGQyNShmPDJkYzcxNzJnY2A2PGRmMzAneCknZ2BxZHYnP15YKSdpZHxqcHFRfHVgJz8naHBpcWxabHFgaCcpJ3dgY2B3d2B3SndsYmxrJz8nbXFxdXY%2FKip2YHdzYHdpYHZ2KHVkfGhga3Era2BxaWxjfCtkdXUneCUl'
+		)
+	}
 
 	return (
 		<div className="border-t border-gray-200 px-4 py-6 sm:px-6">
@@ -17,6 +25,7 @@ const CartActions: FC<{ cart: CartProductItem[]; onClose: () => void }> = ({ car
 					className={`flex items-center w-full justify-center rounded-md border border-transparent hover:from-orange-500 hover:to-orange-700 px-6 py-3 text-base font-medium text-white shadow-sm ${
 						cart?.length > 0 ? 'bg-gradient-to-tr from-orange-400 to-orange-600' : 'bg-gray-400'
 					}`}
+					onClick={handleCheckout}
 				>
 					Checkout
 				</button>
---
+++ src/app/components/Map/index.tsx
@@ -0,0 +1,11 @@
+import { type FC } from 'react'
+
+const Map: FC = () => (
+	<iframe
+		className="absolute top-0 left-0 w-full h-full"
+		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.2956810130822!2d-79.38784734984777!3d43.64212397854147!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34d68bf33a9b%3A0x15edd8c4de1c7581!2sCN%20Tower!5e0!3m2!1sen!2sca!4v1704313456782!5m2!1sen!2sca"
+		loading="lazy"
+	/>
+)
+
+export default Map
