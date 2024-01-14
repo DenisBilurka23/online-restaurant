d55247512fb6bff3fdf3116a2c0fb6559b9ff992
DenisBilurka23 <vadya555555@gmail.com>
2024-01-13 19:12:23 -0500

    image import fix

--- src/app/components/AboutUs/index.tsx
+++ src/app/components/AboutUs/index.tsx
@@ -1,5 +1,5 @@
 import { type FC } from 'react'
-import Image from 'next/Image'
+import Image from 'next/image'
 import MainFeatureProd from '../../../../public/img/main-feature-prod.jpg'
 import SubFeatureProd from '../../../../public/img/sub-feature-prod.jpg'
 
