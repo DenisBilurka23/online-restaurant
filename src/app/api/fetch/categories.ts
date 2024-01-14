fff3964436a95b4608dd0a07f5523ed236c8647a
DenisBilurka23 <vadya555555@gmail.com>
2024-01-13 19:43:31 -0500

    request env variable fix

--- src/app/api/fetch/categories.ts
+++ src/app/api/fetch/categories.ts
@@ -1,4 +1,4 @@
-import { type FoodCategory } from '../../../../types'
+import { type FoodCategories, type FoodCategory } from '../../../../types'
 import { REQUEST } from '@/app/api/fetch/index'
 
 export const getCategory: (categoryId: string | undefined) => Promise<FoodCategory> = async categoryId => {
@@ -6,6 +6,15 @@ export const getCategory: (categoryId: string | undefined) => Promise<FoodCatego
 		return null
 	}
 	const res = await REQUEST(`api/categories/${categoryId}`)
+	if (!res.ok) {
+		throw new Error('Failed to fetch data')
+	}
+
+	return res.json()
+}
+
+export const getCategories: () => Promise<FoodCategories> = async () => {
+	const res = await REQUEST('api/categories')
 	if (!res.ok) {
 		throw new Error('Failed to fetch data')
 	}
